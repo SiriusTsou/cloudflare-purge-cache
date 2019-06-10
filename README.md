e989e0f160312bda81addc6860f2041ca695402b
sirius <sirius.tsou@gmail.com>
2019-06-11 01:39:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cloudflare purge cache
+
+Purge cloudflare all files cache.
+
+## Exmaple
+
+## Setup
+
+## Usage
---
+++ bin/cloudflare-purge-cache.js
@@ -0,0 +1,35 @@
+const cloudflarePurgeCache = require('../lib/index.js')
+
+const argv = require('yargs')
+.option('email', {
+  alias: 'e',
+  describe: 'Cloudflare\'s email'
+})
+.option('key', {
+  alias: 'k',
+  describe: 'Cloudflare\'s API key'
+})
+.option('zone', {
+  alias: 'z',
+  describe: 'Cloudflare\'s Zone ID'
+})
+.option('slient', {
+  alias: 's',
+  describe: 'Slient Mode'
+})
+.help()
+.argv
+
+argv.email = argv.email || argv._[0]
+argv.key = argv.key || argv._[1]
+argv.zone = argv.zone || argv._[2]
+argv.slient = argv.slient || argv._[3]
+
+const slient = (typeof argv.slient !== 'undefined' && argv.slient === "true")
+cloudflarePurgeCache(argv).then(result => {
+  if (!slient) console.log(result)
+  process.exit(0)
+}).catch(e => {
+  if (!slient) console.error(e)
+  process.exit(1)
+})
